db2f4fd10ab185f7e7be42e9b303c504af4f7e55
Venkat-krish <venkat.krishnan@live.com>
2020-04-04 10:32:06 +0530

    Initial commit

---
+++ http_get.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+  "fmt"
+  "io/ioutil"
+  "net/http"
+)
+
+func main(){
+  resp, _ := http.Get("http://example.com/")
+
+  body, _ := ioutil.ReadAll(resp.Body)
+  fmt.Println(string(body))
+  resp.Body.Close()
+  
+}
---
+++ read_status.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+  "fmt"
+  "bufio"
+  "net"
+)
+
+func main(){
+  conn, _ := net.Dial("tcp", "golang.org:80")
+  fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+  status, _ := bufio.NewReader(conn).ReadString('\n')
+
+  fmt.Println(status)
+}
